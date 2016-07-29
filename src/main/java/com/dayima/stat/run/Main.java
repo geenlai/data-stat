04144f1a1a6e1b9bcfb48fb3260004cbe3f47362
geenlai <geenlai@dayima.com>
2016-07-29 16:57:04 +0800

    master rm1

--- src/main/java/com/dayima/stat/run/Main.java
+++ src/main/java/com/dayima/stat/run/Main.java
@@ -23,7 +23,6 @@ public class Main {
 			System.out.println("11");
 			System.out.println("22");
 			System.out.println("33");
-			System.out.println("44");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
