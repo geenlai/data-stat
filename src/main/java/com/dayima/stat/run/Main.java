f7e7dd593ca7ece90c86f59056d762152b33cb46
geenlai <geenlai@dayima.com>
2016-07-29 16:15:23 +0800

    master commit22

--- src/main/java/com/dayima/stat/run/Main.java
+++ src/main/java/com/dayima/stat/run/Main.java
@@ -21,6 +21,7 @@ public class Main {
 			System.out.println("修改22");
 			System.out.println("修改55");
 			System.out.println("11");
+			System.out.println("22");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
